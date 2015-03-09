ce8e29bf4c97e0fa7b68de21e73343e95a3605d6
Greg Smith <gregs@6ft.com>
2015-03-09 15:00:01 -0500

    Add Index Page

--- magic_the_gathering/urls.py
+++ magic_the_gathering/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^$', 'magic_the_gathering.views.index'),
     url(r'^api/', include('magic_the_gathering.api.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
